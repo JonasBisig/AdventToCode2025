b5a4de10f67c3ddd07225327ccead2b62cd0412d
JonasBisig <jonas.bisig@gmx.ch>
2026-01-09 11:27:24 +0100

    Ninth Day Part 1 Finished

--- AdventToCode/Program.cs
+++ AdventToCode/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -24,11 +25,25 @@ namespace AdventToCode
 
             //to use the example string enable this line and comment the line below
             IEnumerable<string> array = example.Split("\r\n");
-            //IEnumerable<string> array = File.ReadLines(Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName, "input.txt"));
+            //IEnumerable<string> array = File.ReadAllLines(Path.Combine(Directory.GetParent(AppContext.BaseDirectory).Parent.Parent.FullName, "input.txt"));
 
-            List<Tuple<int, int>> pos = array.Select(x => (int.Parse(x.Split(",")[0]), int.Parse(x.Split(",")[1])).ToTuple()).ToList();
+            //get every combination of positions and order them by areasize descending -> largest area at pos 0
+            List<Tuple<(double, double), (double, double)>> areas = array.SelectMany((a, i) => array.Skip(i + 1).Select(b => ((double.Parse(a.Split(",")[0]), double.Parse(a.Split(",")[1])), (double.Parse(b.Split(",")[0]), double.Parse(b.Split(",")[1]))).ToTuple()))
+                .OrderByDescending(x => (Math.Abs(x.Item1.Item1 - x.Item2.Item1) + 1) * (Math.Abs(x.Item1.Item2 - x.Item2.Item2) + 1))
+                .ToList();
 
-            List<List<Tuple<int, int>>> positions = array.Select((a, i) => array.Skip(i + 1).Select(b => new List<Tuple<int, int>>([(1,1).ToTuple()])).ToList()).ToList();
+            //get the largest area rectangle
+            double result = (Math.Abs(areas[0].Item1.Item1 - areas[0].Item2.Item1) + 1) * (Math.Abs(areas[0].Item1.Item2 - areas[0].Item2.Item2) + 1);
+
+            Console.WriteLine($"The largest area of any rectangle you can make is {result}!");
+
+            //Different solution -> single line
+            //directly calculate area sizes for every combination and order them descending then only display the first position of the list
+            List<double> areas2 = array.SelectMany((a, i) => array.Skip(i + 1).Select(b => (Math.Abs(double.Parse(a.Split(",")[0]) - double.Parse(b.Split(",")[0])) + 1) * (Math.Abs(double.Parse(a.Split(",")[1]) - double.Parse(b.Split(",")[1])) + 1)))
+                .OrderByDescending(x => x)
+                .ToList();
+
+            Console.WriteLine($"Different approach, same solution: {areas2[0]}");
         }
     }
 }
--- AdventToCode/obj/Debug/net10.0/AdventToCode.AssemblyInfo.cs
+++ AdventToCode/obj/Debug/net10.0/AdventToCode.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AdventToCode")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ae8aa30dfb7b66205586a63a80fed9b4faa396d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72f5164d42dc709bd75260150a32c82ee5955121")]
 [assembly: System.Reflection.AssemblyProductAttribute("AdventToCode")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AdventToCode")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
